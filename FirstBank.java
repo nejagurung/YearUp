e5527004179a701b75956895d9772e3235f68315
Neja Gurung <78182907+nejagurung@users.noreply.github.com>
2022-07-15 22:23:20 -0400

    Java_BankingApplication

---
+++ FirstBank.java
@@ -0,0 +1,12 @@
+package Project;
+
+public class FirstBank {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account Neja = new Account("Neja Gurung", "CIS 404 Java Project");
+        Neja.showMenu();
+
+    }
+}
